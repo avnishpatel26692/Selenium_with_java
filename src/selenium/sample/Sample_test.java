c0c8cba4aefd1c85aaf60ee3f29f425153b664cf
TimG666 <timur.geldiev@gmail.com>
2021-06-07 11:09:54 +0300

    test message

---
+++ src/selenium/sample/Sample_test.java
@@ -0,0 +1,45 @@
+package selenium.sample;
+
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Sample_test {
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+
+    @Test
+    public void goToHomepage() throws Exception {
+        //define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver_91.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //open test homepage
+//        driver.get("https://google.com");
+        driver.get("https://kristinek.github.io/site/");
+
+
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        /*driver.findElement(By.xpath("//input[@aria-label = `Search`]")).sendKeys("some text");
+        driver.findElement(By.xpath("//input[@type = `submit`]")).click();*/
+
+        System.out.println(driver.findElement(By.id("h1")).getText());
+//        //get title of page
+        System.out.println(driver.getTitle());
+//
+//        //get URL of current page
+        System.out.println(driver.getCurrentUrl());
+//
+//        //Sleep for 10 seconds
+        Thread.sleep(5000);
+
+        //Close browser
+        driver.quit();
+    }
+}
