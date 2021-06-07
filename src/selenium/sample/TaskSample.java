36a3f823e43860628ef9ac5f52f7b7c5caf7673e
KristianaSniedzina <kristiana.sniedzina@gmail.com>
2021-06-07 11:09:59 +0300

    Added Sample1 Task.Kristiana Sniedzina

---
+++ src/selenium/sample/TaskSample.java
@@ -0,0 +1,30 @@
+package selenium.sample;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TaskSample {
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+
+    @Test
+    public void goToHomepage() throws Exception {
+        //define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver_91.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://kristinek.github.io/site/");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        //Sleep for 10 seconds
+        Thread.sleep(5000);
+        //Close browser
+        driver.quit();
+    }
+
+}
