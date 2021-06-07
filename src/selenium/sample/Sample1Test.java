19b5e668737429e1f86cbe502f7beeed2a9215c9
Valery Stroikin <vstroiki@mail.ru>
2021-06-07 11:10:17 +0300

    Added Sample1Test.java

---
+++ src/selenium/sample/Sample1Test.java
@@ -0,0 +1,24 @@
+package selenium.sample;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample1Test {
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+    @Test
+    public void goToHomepage() throws Exception {
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver_91.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //open test homepage
+//        driver.get("https://google.com");
+        driver.get("https://kristinek.github.io/site/");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
