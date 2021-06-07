08fa6e842b29f53ee84ca38a74ff8578cdcdea86
EduardNovickij <eduards.novickijs@gmail.com>
2021-06-07 12:42:05 +0300

    Added Sample2(-Task)

---
+++ src/selenium/sample/Sample2.java
@@ -0,0 +1,28 @@
+package selenium.sample;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample2 {
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+
+    @Test
+    public void goToHomepage() throws Exception {
+        //define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver_91.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://kristinek.github.io/site/examples/locators");
+
+        System.out.println(driver.findElement(By.id("heading_1")).getText());
+
+        System.out.println(driver.findElement(By.name("randomButton1")).getAttribute("value"));
+
+        Thread.sleep(5000); //Wait 5 seconds
+
+        driver.close();
+    }
+}
