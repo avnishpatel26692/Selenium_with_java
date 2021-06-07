ca36d2a800bd7b2af78f3dc659d679d63cc5dd9c
JanisBars <janisbars@inbox.lv>
2021-06-07 13:11:03 +0300

    comitih

--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -3,7 +3,11 @@ package selenium.sample;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+import java.util.List;
 
 public class Sample2Task {
 
@@ -21,14 +25,13 @@ public class Sample2Task {
         driver.get("https://kristinek.github.io/site/examples/locators");
 
         System.out.println(driver.findElement(By.id("heading_1")).getText());
+        System.out.println(driver.findElement(By.name("randomButton1")));
     System.out.println(driver.findElement (By.className("twoTest")).getText());
-
-
-
-
-//
-//
-
+        System.out.println(driver.findElement (By.tagName("p")).getText());
+        List<WebElement> pTag = driver.findElements(By.tagName("p"));
+        System.out.println(pTag.size());
+        System.out.println(pTag.get(0).getText());
+        System.out.println(pTag.get(2).getText());
 //        //Sleep for 10 seconds
         Thread.sleep(5000);
 
