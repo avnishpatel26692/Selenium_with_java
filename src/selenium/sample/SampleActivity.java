62d518d200137a31be66930688214e0330b3d5da
Arturs Afanasjevs <artur90@inbox.lv>
2021-06-07 11:07:09 +0300

    Added sample1 task

---
+++ src/selenium/sample/SampleActivity.java
@@ -0,0 +1,27 @@
+package selenium.sample;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SampleActivity {
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+
+    @Test
+    public void goToHomepage() throws Exception {
+        //define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver_91.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://kristinek.github.io/site/");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+}
