96b3251816cfea2de07776d0e8ac9b3706681ca3
Irina Grigorjeva <mrs.igrigoryeva@gmail.com>
2021-06-09 12:40:47 +0300

    Activity Day3, task 2(POM), 9.08.21, Irina Grigoryeva

---
+++ src/selenium/sample/ActivityDayThree/ActivityDayThreePOM/AgepagePOM.java
@@ -0,0 +1,46 @@
+package selenium.sample.ActivityDayThree.ActivityDayThreePOM;
+
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+
+public class AgepagePOM {
+
+    @FindBy(how = How.NAME, using = "name")
+    private WebElement nameTextbox;
+
+    @FindBy(how = How.ID, using = "age")
+    private WebElement ageTextbox;
+
+    @FindBy(how = How.XPATH, using = "//button[text()='Submit']")
+    private WebElement submitBtn;
+
+    @FindBy(how = How.CSS, using = "p.error")
+    private WebElement errorMsg;
+
+
+
+    public void enterName(String name) {
+        nameTextbox.clear();
+        nameTextbox.sendKeys(name);
+    }
+
+    public void enterAge(String age) {
+        ageTextbox.sendKeys(age);
+    }
+
+    public void clickOnSubmitBtn() {
+        submitBtn.click();
+    }
+
+    public String getErrorMsg()
+    {
+        return errorMsg.getText();
+    }
+
+
+}
+
---
+++ src/selenium/sample/ActivityDayThree/ActivityDayThreePOM/SubbmitAgePagePOM.java
@@ -0,0 +1,30 @@
+package selenium.sample.ActivityDayThree.ActivityDayThreePOM;
+
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+
+
+public class SubbmitAgePagePOM {
+    @FindBy(how = How.ID, using = "message")
+    private WebElement message;
+
+    @FindBy(how = How.XPATH, using = "//button[@type='button']")
+    private WebElement backBtn;
+
+   public String getMessage()
+    {
+        return message.getText();
+    }
+
+    public void clickOnBack()
+    {
+        backBtn.click();
+    }
+
+
+
+}
