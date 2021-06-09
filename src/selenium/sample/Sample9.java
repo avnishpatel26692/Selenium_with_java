d2de47072755a7f60d04133b5ec95c0c2d132b1a
Mariia Akhmedova <marry.bellows@gmail.com>
2021-06-09 02:15:43 -0600

    Added Sample9

---
+++ src/selenium/sample/Sample9.java
@@ -0,0 +1,49 @@
+package selenium.sample;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Sample9 {
+
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+    WebDriver driver;
+
+    @Before
+    public void beforeMethod() {
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver_91.exe");
+        driver = new ChromeDriver();
+        driver.get("https://kristinek.github.io/site/examples/loading_color");
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void afterMethod() throws InterruptedException {
+        //Close browser
+        driver.quit();
+    }
+
+    @Test
+    public void greenLoad() {
+        WebElement greenBtn = driver.findElement(By.id("start_green"));
+        greenBtn.click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("finish_green")));
+
+    }
+    @Test
+    public void greenAndBlueLoad(){
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("start_green_and_blue")));
+        WebElement greenAndBlueBtn = driver.findElement(By.id("start_green_and_blue"));
+        greenAndBlueBtn.click();
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("finish_green_and_blue")));
+
+}}
+
