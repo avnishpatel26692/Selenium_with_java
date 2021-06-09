402ca26a0bc1d00f8b4dc5c125f9c07e9b9fbfd1
Margarita Roze <margarita.roze@accenture.com>
2021-06-09 12:12:45 +0300

    Task  12 updated

---
+++ src/selenium/PageObject/AgeSubbmited.java
@@ -0,0 +1,24 @@
+package selenium.PageObject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class AgeSubbmited {
+    @FindBy(how = How.ID, using = "message")
+    private WebElement message;
+
+    @FindBy(how = How.XPATH, using = "//button[@type='button']")
+    private WebElement backBtn;
+
+    public String getMessage()
+    {
+        return message.getText();
+    }
+
+    public void clickOnBack()
+    {
+        backBtn.click();
+    }
+
+}
